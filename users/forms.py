f890b4c8ddf2f213f2bd7053e58eeecb4c7c0924
M_dev_pro <iran2014.mohammad@gmail.com>
2020-09-30 14:27:11 +0330

    inserted users app to INSTALL_APPS in settings.py and defined LoginModelForm and SignupModelForm

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 
-class UserModelForm(ModelForm):
+class LoginModelForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ('username', 'password')
+
+
+class SignupModelForm(ModelForm):
 	class Meta:
 		model = User
 		fields = ('username', 'password')
