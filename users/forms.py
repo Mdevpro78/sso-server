4b72366ece18d2129e638e508c80e2ee43fd73f0
M_dev_pro <iran2014.mohammad@gmail.com>
2020-09-30 17:17:49 +0330

    fixed wrongs about users app forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
 
-class LoginModelForm(ModelForm):
+
+class SignupForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ('username', 'password')
-
-
-class SignupModelForm(ModelForm):
-	class Meta:
-		model = User
-		fields = ('username', 'password')
+		fields = ('username',
+		          'password1',
+		          'password2',
+		          )
